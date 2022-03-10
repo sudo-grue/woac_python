5eda3f8d65356039059bb962b3bce8a4b21145ee
Kyle Matuszczak <kyle.l.matusczak.mil@army.mil>
2022-03-10 11:39:53 -0500

    Modified template to be slightly clearer to task

--- challenge_6/challenge_6.py
+++ challenge_6/challenge_6.py
@@ -4,24 +4,22 @@ Challenge 6 Module
 TASK:
 Create a class object named 'Sphere'
   - Takes one (1) initializing argument float() or int() representing radius
-  - Must have four (4) methods
-    - get_surface_area(): returns surface area of sphere
-    - get_volume()      : returns volume of sphere
-    - get_radius()      : returns the numeric value stored in radius
-    - set_radius()      : sets (updates) the radius
+  - radius will be stored as a private ('__') instance attribute
+  - Must have five (5) methods
+    - get_surface_area() : returns surface area of sphere
+    - get_volume()       : returns volume of sphere
+    - radius()           : use @property decorator as getter
+    - radius()           : use @radius.setter decorator as setter
+    - __str__()          : used by print() to override default
 
   - When radius is updated, resulting surface area and volume will change
   - print() should yield the current radius
   - if set_radius() receives invalid input, do not update values
+  - On creation, raise ValueError if invalid input
 
 Note:
   To standardize calculations pi will be calculated to a precision of 2
-
-INFO:
-  Surface area formula: 4 * pi * (r ^ 2)
-  Volume formula      : (4/3) * pi * (r ^ 3)
 """
-
 from typing import Union
 
 
@@ -37,12 +35,31 @@ class Sphere():
     Sphere class containing four (4) methods:
     - get_surface_area()
     - get_volume()
-    - get_radius()
-    - set_radius()
+    - radius() getter
+    - radius() setter
+    - __str__()
     """
     pi = 3.14
 
     def __init__(self, radius: Union[int, float]):
+        self.__radius = radius
+        pass
+
+    @property
+    def radius(self):
+        pass
+
+    @radius.setter
+    def radius(self, radius):
+        pass
+
+    def get_surface_area(self):
+        pass
+
+    def get_volume(self):
+        pass
+
+    def __str__(self):
         pass
 
 
