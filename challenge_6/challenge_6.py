f52c49a3a70375b4ead2c83d0cdb661b674da1a2
Kyle Matuszczak <kyle.l.matusczak.mil@army.mil>
2022-03-11 17:00:52 -0500

    Modified comments for clarity

--- challenge_6/challenge_6.py
+++ challenge_6/challenge_6.py
@@ -7,36 +7,41 @@ Create a class object named 'Sphere'
   - Takes one (1) initializing argument float() or int() representing radius
   - radius will be stored as a private __radius instance attribute
   - Must have following methods
+    - "secret" method   : prevents additional members (HINT: class attribute)
+
+    - __init__()        : used when "creation" of object happens
+
+    - radius()          : use @property decorator as getter
+    - radius()          : use @radius.setter decorator as setter
+
     - get_surface_area(): returns surface area of sphere
     - get_volume()      : returns volume of sphere
-    - radius()          : use @property decorator as getter 
-    - radius()          : use @radius.setter decorator as setter
-    - __init__()        : used when "creation" of object happens
-    - __lt__()          : less than method for comparisons
-    - __le__()          : less than or equal...
-    - __eq__()          : equal
-    - __ne__()          : not equal
-    - __gt__()          : greater than
-    - __ge__()          : greater than or equal
+
     - __str__()         : used by print() to override default
     - __repr__()        : effectively a "How would you recreate this object?"
-    - "secret" method   : prevents additional members (HINT: class attribute)
-  BONUS:
-    - __add__()         : Returns Sphere + (int, float, Sphere) -> (int, float)
-    - __radd__()        : Returns (int, float) + Sphere -> (int, float)
-    - __sub__()         : Returns Sphere - (int, float, Sphere) -> (int, float)
-    - __rsub__()        : Returns (int, float) - Sphere -> (int, float)
+
+    - __lt__()          : Sphere <  (int, float, Sphere) -> bool
+    - __le__()          : Sphere <= (int, float, Sphere) -> bool
+    - __eq__()          : Sphere == (int, float, Sphere) -> bool
+    - __ne__()          : Sphere != (int, float, Sphere) -> bool
+    - __gt__()          : Sphere >  (int, float, Sphere) -> bool
+    - __ge__()          : Sphere >= (int, float, Sphere) -> bool
+
+    - __add__()         : Sphere + (int, float, Sphere) -> (int, float)
+    - __sub__()         : Sphere - (int, float, Sphere) -> (int, float)
+    - __radd__()        : (int, float) + Sphere         -> (int, float)
+    - __rsub__()        : (int, float) - Sphere         -> (int, float)
 
   - When radius is updated, resulting surface area and volume will change
   - print() should yield the current radius
-  - object.radius setter will not change value on invalid input
+  - object.radius will not change value on invalid input (no error raised)
   - On creation, raise ValueError if invalid input
 
 Note:
   To standardize calculations pi will be calculated to a precision of 2
 
   WARNING: Some methods may "wrongly" succeed until "secret" method implemented
-  
+
   See https://docs.python.org/3/reference/datamodel.html#basic-customization
     for details about all dunder methods (including "secret")
 """
